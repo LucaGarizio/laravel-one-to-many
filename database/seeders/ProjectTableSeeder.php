aef83a3927b625c9734ec5702c9977e9ee33f6bd
Luca Garizio <garizioluca2@gmail.com>
2024-02-22 17:39:15 +0100

    add relation between tables

--- database/seeders/ProjectTableSeeder.php
+++ database/seeders/ProjectTableSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Project;
+use App\Models\Type;
 
 class ProjectTableSeeder extends Seeder
 {
@@ -15,6 +16,16 @@ class ProjectTableSeeder extends Seeder
      */
     public function run()
     {
-        Project :: factory() -> count(5) -> create();
+       
+        Project :: factory()
+            -> count(50)
+            -> make()
+            -> each(function($project) {
+
+            $type = Type :: inRandomOrder() -> first();
+            $project -> type() -> associate($type);
+
+            $project -> save();
+        });
     }
 }
